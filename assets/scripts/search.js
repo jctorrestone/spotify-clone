8cf96a767dd02e9fe75da5af91b8e66c52a5b49e
Jose <jc95lb@gmail.com>
2023-02-13 11:51:30 -0500

    browse all done

---
+++ assets/scripts/search.js
@@ -0,0 +1,254 @@
+const browseAllItems = [
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+        backgroundColor: "#E8115B",
+        title: "Nuevos lanzamientos",
+        href: "/new-releases-page"
+    },
+    {
+        artwork: "https://i.scdn.co/image/ab67fb8200005cafa59f90c077c9f618fd0dde30",
+        backgroundColor: "#e1118c",
+        title: "Latina",
+        href: "/genre/0JQ5DAqbMKFxXaXKP7zcDp"
+    },
+    {
+        artwork: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+        backgroundColor: "#e13300",
+        title: "Podcasts",
+        href: "/genre/0JQ5DArNBzkmxXHCqFLx2J"
+    },
+    {
+        artwork: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+        backgroundColor: "#1E3264",
+        title: "Creadas para vos",
+        href: "/made-for-you"
+    }
+];
+
+const gridContainerDiv = document.querySelector(".grid-container");
+const recentSearchDiv = document.querySelector(".recent-search");
+const browseAllDiv = document.querySelector(".browse-all");
+
+browseAllDiv.innerHTML = browseAllItems.map((item)=>{
+    return `<a class="grid-item" style="background-color:${item.backgroundColor};" href="${item.href}">
+                <h3>${item.title}</h3>
+                <img class="item-img" src="${item.artwork}" alt="${item.title}">
+            </a>`;
+}).join(" ");
+
+function setGaps() {
+    if(window.innerWidth > 973){
+        gridContainerDiv.style.setProperty("gap", "24px");
+        recentSearchDiv.style.setProperty("gap", "24px");
+        browseAllDiv.style.setProperty("gap", "24px");
+    }
+    else if (window.innerWidth > 772){
+        gridContainerDiv.style.setProperty("gap", "18px");
+        recentSearchDiv.style.setProperty("gap", "18px");
+        browseAllDiv.style.setProperty("gap", "18px");
+    }
+    else{
+        gridContainerDiv.style.setProperty("gap", "12px");
+        recentSearchDiv.style.setProperty("gap", "12px");
+        browseAllDiv.style.setProperty("gap", "12px");
+    }
+}
+
+function setColumnCount() {
+    if(window.innerWidth > 1709){
+        gridContainerDiv.style.setProperty("--column-count", 7);
+        recentSearchDiv.style.setProperty("--column-count", 7);
+        browseAllDiv.style.setProperty("--column-count", 7);
+    }
+    else if (window.innerWidth > 1504){
+        gridContainerDiv.style.setProperty("--column-count", 6);
+        recentSearchDiv.style.setProperty("--column-count", 6);
+        browseAllDiv.style.setProperty("--column-count", 6);
+    }
+    else if (window.innerWidth > 1301){
+        gridContainerDiv.style.setProperty("--column-count", 5);
+        recentSearchDiv.style.setProperty("--column-count", 5);
+        browseAllDiv.style.setProperty("--column-count", 5);
+    }
+    else if (window.innerWidth > 973){
+        gridContainerDiv.style.setProperty("--column-count", 4);
+        recentSearchDiv.style.setProperty("--column-count", 4);
+        browseAllDiv.style.setProperty("--column-count", 4);
+    }
+    else if (window.innerWidth > 772){
+        gridContainerDiv.style.setProperty("--column-count", 3);
+        recentSearchDiv.style.setProperty("--column-count", 3);
+        browseAllDiv.style.setProperty("--column-count", 3);
+    }
+    else{
+        gridContainerDiv.style.setProperty("--column-count", 2);
+        recentSearchDiv.style.setProperty("--column-count", 2);
+        browseAllDiv.style.setProperty("--column-count", 2);
+    }
+}
+
+onload = () => {
+    setGaps();
+    setColumnCount();
+}
+
+onresize = () => {
+    setGaps();
+    setColumnCount();
+}
